815f6f0f5d0973004e0d65fea54b4c15361a270d
James Aprosail <aprosail@outlook.com>
2024-06-27 11:31:32 +0800

    fix ci exit 1

--- review.sh
+++ review.sh
@@ -1,5 +1,5 @@
-flutter pub get
-dart format --output=none --set-exit-if-changed .
-flutter analyze --fatal-infos
-flutter test
-flutter pub publish --dry-run
+flutter pub get || exit 1
+dart format --output=none --set-exit-if-changed . || exit 1
+flutter analyze --fatal-infos || exit 1
+flutter test || exit 1
+flutter pub publish --dry-run || exit 1
