96eeb522626ed66b391c39e3e744c967cd36d940
James Aprosail <aprosail@outlook.com>
2024-06-28 18:17:08 +0800

    test example common type wrap

--- test/inherit_test.dart
+++ test/inherit_test.dart
@@ -3,16 +3,45 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:modifier/modifier.dart';
 
 void main() {
-  testWidgets('inherit and find', (t) async {
-    const message = 'it works';
-    await t.pumpWidget(
-      builder(
-        (context) => Text(context.findAndTrust<String>())
+  testFindAndTrust();
+}
+
+void testFindAndTrust() {
+  group('find and trust', () {
+    // It's strongly not recommended to code like that,
+    // because there might many inherited data with the String type
+    // in that case, and it will only return the closest one,
+    // the they might shadow each other.
+    // Please refer to the test below.
+    testWidgets('find and trust', (t) async {
+      const message = 'it works';
+      await t.pumpWidget(
+        builder((context) => Text(context.findAndTrust<String>())
             .center
             .ensureDirection(context)
-            .ensureMedia(context),
-      ).inherit(message),
-    );
-    expect(find.text(message), findsOneWidget);
+            .ensureMedia(context)).inherit(message),
+      );
+      expect(find.text(message), findsOneWidget);
+    });
+
+    testWidgets('find and trust typed', (t) async {
+      const message = 'it works';
+      await t.pumpWidget(
+        builder((context) =>
+                Text(context.findAndTrust<MessageExample>().message)
+                    .center
+                    .ensureDirection(context)
+                    .ensureMedia(context))
+            .inherit(const MessageExample(message: message)),
+      );
+    });
   });
+}
+
+/// Wrapping a single text message for demonstration.
+/// See the code inside [testFindAndTrust].
+class MessageExample {
+  const MessageExample({required this.message});
+
+  final String message;
 }
