b5c8e65638aa951f2ad4b75ca287b9b0da458d97
James Aprosail <aprosail@outlook.com>
2024-06-29 21:56:36 +0800

    format simplify testing code using modifier test package

--- test/wrap_test.dart
+++ test/wrap_test.dart
@@ -4,57 +4,44 @@ import 'package:modifier/modifier.dart';
 import 'package:modifier_test/modifier_test.dart';
 
 void main() {
-  testText();
-}
-
-void testText() {
-  testWidgets('builder', (t) async {
+  testWidgets('multi nested builder', (t) async {
+    /// Encapsulation for update platform brightness,
+    /// to make testing code conciser.
     Widget updateBrightness(
       BuildContext context,
       Widget child,
       Brightness brightness,
-    ) =>
-        child.updateMedia(
-          context,
-          (raw) => raw.copyWith(platformBrightness: brightness),
-        );
+    ) {
+      return child.updateMedia(context, (raw) {
+        return raw.copyWith(platformBrightness: brightness);
+      });
+    }
 
-    await builder(
-      (context) {
-        final brightness = MediaQuery.of(context).platformBrightness;
-        return 'brightness: ${brightness.name}'.asText;
-      },
-    )
-        .center
+    await builder((context) =>
+            'brightness: ${MediaQuery.of(context).platformBrightness.name}'
+                .asText
+                .center)
         .builder((c, child) => updateBrightness(c, child, Brightness.dark))
         .builder((c, child) => updateBrightness(c, child, Brightness.light))
-        .builder(
-          (context, child) => child
-              .ensureDirection(context)
-              .ensureMedia(context), // Line break.
-        )
+        .builder((c, child) => child.ensureDirection(c).ensureMedia(c))
         .pump(t);
 
     expect(find.text('brightness: dark'), findsOneWidget);
   });
 
-  group('text', () {
+  group('ensure text display', () {
     testWidgets('enable text', (t) async {
-      await t.pumpWidget(
-        builder((context) => const Text('validation')
-            .ensureDirection(context)
-            .ensureMedia(context)),
-      );
+      await builder((context) => const Text('validation')
+          .ensureDirection(context)
+          .ensureMedia(context)).pump(t);
       expect(find.text('validation'), findsOneWidget);
     });
 
     testWidgets('wrap text', (t) async {
-      await t.pumpWidget(
-        builder((context) => 'convert string into text'
-            .asText
-            .ensureDirection(context)
-            .ensureMedia(context)),
-      );
+      await builder((context) => 'convert string into text'
+          .asText
+          .ensureDirection(context)
+          .ensureMedia(context)).pump(t);
       expect(find.text('convert string into text'), findsOneWidget);
     });
   });
