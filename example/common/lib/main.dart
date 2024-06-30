e5e4a14a85c5ee36760298d61b67c5a3306e4cfd
James Aprosail <aprosail@outlook.com>
2024-06-30 23:36:19 +0800

    example apply theme

--- example/common/lib/main.dart
+++ example/common/lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:color_chart/color_chart.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:modifier/modifier.dart';
 
 void main() {
   runApp(const App());
@@ -9,6 +12,38 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return builder((context) {
+      final platformBrightness = MediaQuery.of(context).platformBrightness;
+      final brightness = context.findAndTrust<Brightness>();
+      final theme = context.findAndTrust<ThemeData>();
+      return [
+        'Platform brightness: ${platformBrightness.name}'.asText,
+        'Theme brightness: ${brightness.name}'.asText,
+        'Foreground: ${theme.foreground.hex}'.asText,
+        'Background: ${theme.background.hex}'.asText,
+      ].asColumn;
+    })
+        .center
+        .theme(light: const ThemeData.light(), dark: const ThemeData.dark())
+        .ensureDirection(context)
+        .ensureMedia(context);
   }
+}
+
+class ThemeData with Theme {
+  const ThemeData.light({
+    this.background = MonoColors.snow,
+    this.foreground = MonoColors.ink,
+  });
+
+  const ThemeData.dark({
+    this.background = MonoColors.night,
+    this.foreground = MonoColors.lunar,
+  });
+
+  @override
+  final Color background;
+
+  @override
+  final Color foreground;
 }
